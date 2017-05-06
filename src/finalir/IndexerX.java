ff74c0b376ca3abb390ae96f43359f2b138cc1e8
bhlshrf <bhlshrf@gmail.com>
2017-05-06 12:40:23 +0300

    adding the boolean search

--- src/finalir/IndexerX.java
+++ src/finalir/IndexerX.java
@@ -6,6 +6,8 @@ import static finalir.IR.Print;
 import java.io.File;
 import java.io.IOException;
 import static java.lang.Math.sqrt;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -93,7 +95,7 @@ public class IndexerX{
             .IndexText("this is the project to solve your information retrivel problems, enjoy it and have a good day")
             .IndexText("the sun is not yellow and the sky isn't realy blue, red red red, the end");
 
-        Print("AllDocumentsCount: \t" +  indx.index.getAllDocumentsCount());
+        Print("AllDocumentsCount: \t" +  indx.index.getCountOfDocuments());
         Print("AvaregeDocumentLength: \t" +  indx.index.getAvaregeDocumentLength());
         Print("SiqmaTF(\"sun\"): \t" +  indx.index.getSiqmaTF("sun"));
         Print("DF(\"sun\"): \t\t" +  indx.index.getDF("sun"));
@@ -104,22 +106,111 @@ public class IndexerX{
 
         Print("Stopwrods removed: \t" + indx.toky.getRemovedWordsCount());
         
-        Print("");
+        Print(" ---- 1 ---- ");
         Print("" + indx.index.search("orjkgojrgojsrkmlkgmkdsmg").size());
-        Print("" + indx.index.search("sun").size());
         Print("" + indx.index.search("SUN").size());
         Print("" + indx.index.search("the").size());
         Print("" + indx.index.search("problem").size());
                 
+        Print(" ---- 2 ---- ");
+        Print("" + indx.SearchAnd("sun","blue").size());
+        Print("" + indx.SearchAnd("sun","eye").size());
+        Print("" + indx.SearchOr("sun","eye").size()); 
+        Print("" + indx.SearchNot("eye","sun").size());
+        Print("" + indx.SearchNot("sun","eye").size());
+        Print("" + indx.SearchNot("sun","red").size());
+        Print("" + indx.SearchAnd("sky","blue").size());
+        Print("" + indx.SearchNot(indx.SearchAnd("sky","blue"), "red").size());
     }
     
-    public List<DocumentTermEntry> SearchQuery(String query){
-    
+    public Collection<DocumentTermEntry> SearchQuery(String query){
+        
+        // to-do later: complete this method
+        // first parse the query and execute
+        //
+        
+        for (CoreLabel w : toky.getTokens(query))
+            search(w.lemma());
+        
+        return null;
     }
-    private List<DocumentTermEntry> Search(String term){
+
+    public List<Document> SearchOr(String term1,String term2){                  // make this private
+        return Or(search(term1), search(term2));
+    }
+    
+    public List<Document> SearchOr(String term1,List<Document> d){              // make this private
+        return Or(search(term1), d);
+    }
+    
+    public List<Document> SearchAnd(String term1,String term2){                 // make this private
+        return And(search(term1),search(term2));
+    }
+    
+    public List<Document> SearchAnd(String term1,List<Document> d){             // make this private
+        return And(search(term1),d);
+    }
+    
+    public List<Document> SearchNot(String term1,String term2){                 // make this private
+        return Not(search(term1),search(term2));
+    }
+    
+    public List<Document> SearchNot(String term1,List<Document> d){             // make this private
+        return Not(search(term1),d);
+    }
+    
+    public List<Document> SearchNot(List<Document> d,String term2){             // make this private
+        return Not(d,search(term2));
+    }
+    
+    private List<Document> Or(List<Document> c1,List<Document> c2){
+        List<Document> unine = new ArrayList<>();
         
-        // lemma
-        // search
+        for (Document d : c1)
+            unine.add(d);
+        for (Document d : c2)
+            unine.add(d);
         
+        return unine;
+    }
+    
+    private List<Document> And(List<Document> c1,List<Document> c2){
+        
+        List<Document> intersection = new ArrayList<>();
+        
+        int p1 = 0 , p2 = 0, compare = 0;
+        while(p1 < c1.size() && p2 < c2.size()){
+            compare = c1.get(p1).compareWith(c2.get(p2));
+            
+            if(compare > 0)
+                p2++;
+            else if(compare < 0)
+                p1++;
+            else
+            {
+                intersection.add(c1.get(p1));
+                p1++;
+                p2++;
+            }
+        }
+        
+        return intersection;
+    }
+    
+    private List<Document> Not(List<Document> c1,List<Document> c2){
+        
+        List<Document> not = new ArrayList<>();
+        
+        for (Document d : c1)
+            if(c2.contains(d) == false)
+                not.add(d);
+        
+        return not;
+    }
+    
+    private List<Document> search(String term){
+        List<Document> docs = new ArrayList<>();
+        index.search(term).forEach(d -> docs.add(d.getDocument()));
+        return docs;
     }
 }
